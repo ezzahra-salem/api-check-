ad3c23bbfc8e3c9791ccd49862e83bbe1d239c76
ezzahra-salem <120910745+ezzahra-salem@users.noreply.github.com>
2023-12-23 21:29:05 +0100

    first commit

---
+++ api-check/src/UserList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        setListOfUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
+  return (
+    <div className="user-list">
+      <h2>List of Users</h2>
+      <ul>
+        {listOfUsers.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
